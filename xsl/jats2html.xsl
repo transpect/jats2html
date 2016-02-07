transpect/jats2html

commit 69d59dbe089ef9394496db9b764b8891cae79fed

add list types

xsl/jats2html.xsl
@@ -543,13 +543,13 @@
   <xsl:template match="*:dd/*:label" mode="clean-up"/>
    
   
-  <xsl:template match="list[@list-type eq 'bullet']" mode="jats2html">
+  <xsl:template match="list[@list-type eq 'simple|ndash|bullet']" mode="jats2html">
     <ul>
       <xsl:apply-templates mode="#current"/>
     </ul>
   </xsl:template>
   
-  <xsl:template match="list[matches(@list-type, '^(order|alpha|roman)')]" mode="jats2html">
+  <xsl:template match="list[matches(@list-type, '^(order|alpha|roman|alpha-lower|alpha-upper|roman-lower|roman-upper)')]" mode="jats2html">
     <ol>
       <xsl:apply-templates select="@*, node()" mode="#current"/>
     </ol>

commit 7c71c7ac8e04e9b16d6c35be19c7dfc870c2353a

fix list→ul conversion bug introduced in 69d59db

xsl/jats2html.xsl
@@ -543,7 +543,7 @@
   <xsl:template match="*:dd/*:label" mode="clean-up"/>
    
   
-  <xsl:template match="list[@list-type eq 'simple|ndash|bullet']" mode="jats2html">
+  <xsl:template match="list[matches(@list-type, 'simple|ndash|bullet')]" mode="jats2html">
     <ul>
       <xsl:apply-templates mode="#current"/>
     </ul>